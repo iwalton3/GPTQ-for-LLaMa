e615e3af9c4c24381cf00c3761ed47b108eb36e5
Ian Walton <ian@iwalton.com>
2023-05-07 17:04:08 -0400

    Improve startup time and possibly handle bias issue.

--- src/gptq_llama/quant.py
+++ src/gptq_llama/quant.py
@@ -19,20 +19,20 @@ class Quantizer(nn.Module):
 
     def configure(
         self,
-        bits, perchannel=False, sym=True, 
+        bits, perchannel=False, sym=True,
         mse=False, norm=2.4, grid=100, maxshrink=.8,
         trits=False
         ):
-        
+
         self.maxq = torch.tensor(2 ** bits - 1)
         self.perchannel = perchannel
         self.sym = sym
         self.mse = mse
         self.norm = norm
         self.grid = grid
-        self.maxshrink = maxshrink 
+        self.maxshrink = maxshrink
         if trits:
-            self.maxq = torch.tensor(-1) 
+            self.maxq = torch.tensor(-1)
 
     def find_params(self, x, weight=False):
         dev = x.device
@@ -79,7 +79,7 @@ class Quantizer(nn.Module):
         if self.mse:
             best = torch.full([x.shape[0]], float('inf'), device=dev)
             for i in range(int(self.maxshrink * self.grid)):
-                p = 1 - i / self.grid 
+                p = 1 - i / self.grid
                 xmin1 = p * xmin
                 xmax1 = p * xmax
                 scale1 = (xmax1 - xmin1) / self.maxq
@@ -112,7 +112,7 @@ class Quantizer(nn.Module):
             self.zero = self.zero.reshape((1, -1, 1, 1))
         if len(shape) == 3:
             self.scale = self.scale.reshape((1, 1, -1))
-            self.zero = self.zero.reshape((1, 1, -1)) 
+            self.zero = self.zero.reshape((1, 1, -1))
         if len(shape) == 2:
             self.scale = self.scale.unsqueeze(0)
             self.zero = self.zero.unsqueeze(0)
@@ -135,8 +135,8 @@ except:
     print('CUDA extension not installed.')
 
 # Assumes layer is perfectly divisible into 256 * 256 blocks
-class QuantLinear(nn.Module): 
-    def __init__(self, bits, groupsize, infeatures, outfeatures, faster=False):
+class QuantLinear(nn.Module):
+    def __init__(self, bits, groupsize, infeatures, outfeatures, bias, faster=False, kernel_switch_threshold=128):
         super().__init__()
         if bits not in [2,3,4,8]:
             raise NotImplementedError("Only 2,3,4,8 bits are supported.")
@@ -148,8 +148,11 @@ class QuantLinear(nn.Module):
         groupsize = groupsize if groupsize != -1 else infeatures
         self.groupsize = groupsize
         self.register_buffer('qzeros', torch.zeros((math.ceil(infeatures/groupsize),outfeatures // 256 * (bits * 8)), dtype=torch.int))
-        self.register_buffer('scales', torch.zeros((math.ceil(infeatures/groupsize),outfeatures)))
-        self.register_buffer('bias', torch.zeros(outfeatures))
+        self.register_buffer('scales', torch.zeros((math.ceil(infeatures/groupsize),outfeatures), dtype=torch.float16))
+        if bias:
+            self.register_buffer('bias', torch.zeros((outfeatures),dtype=torch.float16))
+        else:
+            self.bias = None
         self.register_buffer(
             'qweight', torch.zeros((infeatures // 32 * bits, outfeatures), dtype=torch.int)
         )
@@ -159,6 +162,19 @@ class QuantLinear(nn.Module):
         self.half_indim = self.infeatures // 2
         self._initialized_quant_state = False
         self.faster = faster
+        # kernel_switch_threshold is the cutoff input size after which matmul
+        # is performed by unpacking the weights and using torch.matmul
+        self.kernel_switch_threshold = kernel_switch_threshold
+        if isinstance(self.kernel_switch_threshold, bool):
+            self.kernel_switch_threshold = 128 if self.kernel_switch_threshold else None
+        if not self.kernel_switch_threshold is None:
+            # Buffers for bit shifting weight unpacking performed by unpacking the weights and using torch.matmul
+            if self.bits in [2,4,8]:
+                self.register_buffer('wf',torch.tensor(list(range(0,32,self.bits)), dtype=torch.int32).unsqueeze(0),persistent=False)
+            elif self.bits == 3:
+                self.register_buffer('wf', torch.tensor([[0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 0],
+                                                        [0, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31],
+                                                        [0, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 0],], dtype=torch.int32).reshape(1,3,12), persistent=False)
 
     def pack(self, linear, scales, zeros, g_idx=None):
         self.g_idx = (g_idx.clone() if g_idx is not None else self.g_idx).to(torch.int)
@@ -265,11 +281,69 @@ class QuantLinear(nn.Module):
         else:
             y = self.bias.clone().repeat(x.shape[0], 1)
 
+        # from https://github.com/qwopqwop200/GPTQ-for-LLaMa/blob/cuda/quant.py
+        # main kernel is very slow for big initial inputs
+        if not self.kernel_switch_threshold is None and x.shape[0] > self.kernel_switch_threshold:
+            if self.bits in [2,4,8]:
+                zeros = torch.bitwise_right_shift(torch.unsqueeze(self.qzeros, 2).expand(-1, -1, 32 // self.bits), self.wf.unsqueeze(0)).to(torch.int16 if self.bits == 8 else torch.int8)
+                torch.bitwise_and(zeros, (2 ** self.bits) - 1, out=zeros)
+
+                zeros = zeros + 1
+                zeros = zeros.reshape(self.scales.shape)
+
+                weight = torch.bitwise_right_shift(torch.unsqueeze(self.qweight, 1).expand(-1, 32 // self.bits, -1), self.wf.unsqueeze(-1)).to(torch.int16 if self.bits == 8 else torch.int8)
+                torch.bitwise_and(weight,(2 ** self.bits) - 1, out=weight)
+            elif self.bits == 3:
+                zeros = self.qzeros.reshape(self.qzeros.shape[0], self.qzeros.shape[1]//3, 3, 1).expand(-1, -1, -1, 12)
+                zeros = (zeros >> self.wf.unsqueeze(0))
+                zeros[:,:,0,10] = (zeros[:,:,0,10]&0x3) | ((zeros[:,:,1,0] << 2)&0x4)
+                zeros[:,:,1,11] = (zeros[:,:,1,11]&0x1) | ((zeros[:,:,2,0] << 1)&0x6)
+                zeros = zeros & 0x7
+                zeros = torch.cat([zeros[:,:,0,:11], zeros[:,:,1,1:12], zeros[:,:,2,1:11]], dim=2)
+
+                zeros = zeros + 1
+                zeros = zeros.reshape(self.scales.shape)
+
+                weight = self.qweight.reshape(self.qweight.shape[0]//3, 3, 1, self.qweight.shape[1]).expand(-1, -1, 12, -1)
+                weight = (weight >> self.wf.unsqueeze(-1))&0x7
+                weight[:,0,10] = (weight[:,0,10]&0x3) | ((weight[:,1,0] << 2)&0x4)
+                weight[:,1,11] = (weight[:,1,11]&0x1) | ((weight[:,2,0] << 1)&0x6)
+                weight = weight & 0x7
+                weight = torch.cat([weight[:,0,:11], weight[:,1,1:12], weight[:,2,1:11]], dim=1)
+
+            weight = weight.reshape(weight.shape[0] * weight.shape[1], weight.shape[2])
+            num_itr = self.g_idx.shape[0]//x.shape[-1]
+            if num_itr == 1:
+                weights = (self.scales[self.g_idx.long()] * (weight - zeros[self.g_idx.long()]))
+            else:
+                num_dim = self.g_idx.shape[0]//num_itr
+                weights = []
+                for i in range(num_itr):
+                    scale_i = self.scales[:,i*num_dim:(i+1)*num_dim]
+                    weight_i = weight[:,i*num_dim:(i+1)*num_dim]
+                    zeros_i = zeros[:,i*num_dim:(i+1)*num_dim]
+                    g_idx_i = self.g_idx[i*num_dim:(i+1)*num_dim]
+                    weights.append(scale_i[g_idx_i.long()] * (weight_i - zeros_i[g_idx_i.long()]))
+                weights = torch.cat(weights,dim=1)
+            out = torch.matmul(x.half(), weights)
+            out_shape = x.shape[:-1] + (self.outfeatures, )
+            out = out.reshape(out_shape)
+            out = out + self.bias if self.bias is not None else out
+            return out
+
+        outshape = list(x.shape)
+        outshape[-1] = self.outfeatures
+        x = x.reshape(-1, x.shape[-1])
+        if self.bias is None:
+            y = torch.zeros(x.shape[0], outshape[-1], dtype=torch.float32, device=x.device)
+        else:
+            y = self.bias.clone().repeat(x.shape[0], 1)
+
         output_dtype = x.dtype
         if self.faster:
             x = x.half()
-            if self.scales.dtype != torch.float16:
-                self.scales = self.scales.half()
+            if y.dtype != torch.float16:
+                y = y.half()
             if self.bits == 3:
                 quant_cuda.vecquant3matmul_faster(x, self.qweight, y, self.scales, self.qzeros, self.groupsize, self.half_indim)
             elif self.bits == 4:
@@ -299,8 +373,6 @@ def make_quant(module, names, bits, groupsize, faster=True, name=''):
         name1 = name + '.' + attr if name != '' else attr
         if name1 in names:
             delattr(module, attr)
-            setattr(
-                module, attr, QuantLinear(bits, groupsize, tmp.in_features, tmp.out_features, faster=faster)
-            )
+            setattr(module, attr, QuantLinear(bits, groupsize, tmp.in_features, tmp.out_features, tmp.bias is not None, faster=faster))
     for name1, child in module.named_children():
         make_quant(child, names, bits, groupsize, faster, name + '.' + name1 if name != '' else name1)
